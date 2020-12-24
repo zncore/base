854cc83765ba50bc288f93b494e381eff29a0213
Vitaliy <theyamshikov@yandex.ru>
2020-12-24 12:49:05 +0600

    upd

--- src/Enums/StatusEnum.php
+++ src/Enums/StatusEnum.php
@@ -1,23 +1,50 @@
 <?php
 
-//namespace Packages\Reference\Yii2\Web\Enums;
 namespace ZnCore\Base\Enums;
 
-use ZnCore\Base\Interfaces\GetLabelsInterface;
 use ZnCore\Base\Libs\I18Next\Facades\I18Next;
 
-class StatusEnum implements GetLabelsInterface
+class StatusEnum
 {
 
-    const ENABLED = 1;
+    // удален
+    const DELETED = -10;
+
+    /**
+     * @deprecated
+     * @see DISABLED
+     */
+    const DISABLE = 0;
+
+    // отключен
     const DISABLED = 0;
-    const DELETED = self::DISABLED;
+
+    // отвергнут / отклонен
+    const REJECTED = 10;
+
+    // заблокирован
+    const BLOCKED = 20;
+
+    // ожидает одобрения / премодерация
+    const WAIT_APPROVING = 90;
+
+    /**
+     * @deprecated
+     * @see ENABLED
+     */
+    const ENABLE = 100;
+
+    // включен / одобрен
+    const ENABLED = 100;
+
+    // обработано / завершено
+    const COMPLETED = 200;
 
     public static function getLabels()
     {
         return [
-            self::ENABLED => I18Next::t('core', 'status.enabled'),
-            self::DISABLED => I18Next::t('core', 'status.disabled'),
+            self::ENABLED => I18Next::t('app', 'status.enabled'),
+            self::DISABLED => I18Next::t('app', 'status.disabled'),
         ];
     }
 }
