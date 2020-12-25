951fb99f096a530e247886adc03ccc67ba969855
Vitaliy <theyamshikov@yandex.ru>
2020-12-25 20:11:17 +0600

    upd

--- src/Enums/StatusEnum.php
+++ src/Enums/StatusEnum.php
@@ -2,14 +2,22 @@
 
 namespace ZnCore\Base\Enums;
 
+use ZnCore\Base\Libs\I18Next\Facades\I18Next;
+
 class StatusEnum
 {
 
     // удален
     const DELETED = -10;
 
-    // отключен
+    /**
+     * @deprecated
+     * @see DISABLED
+     */
     const DISABLE = 0;
+
+    // отключен
+    const DISABLED = 0;
 
     // отвергнут / отклонен
     const REJECTED = 10;
@@ -20,10 +28,23 @@ class StatusEnum
     // ожидает одобрения / премодерация
     const WAIT_APPROVING = 90;
 
-    // включен / одобрен
+    /**
+     * @deprecated
+     * @see ENABLED
+     */
     const ENABLE = 100;
+
+    // включен / одобрен
+    const ENABLED = 100;
 
     // обработано / завершено
     const COMPLETED = 200;
 
+    public static function getLabels()
+    {
+        return [
+            self::ENABLED => I18Next::t('app', 'status.enabled'),
+            self::DISABLED => I18Next::t('app', 'status.disabled'),
+        ];
+    }
 }
