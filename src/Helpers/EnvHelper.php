51c6913da71245fb1ac25db80e1ee6070a8b7a06
Vitaliy <theyamshikov@yandex.ru>
2020-12-31 16:17:38 +0600

    upd

--- src/Helpers/EnvHelper.php
+++ src/Helpers/EnvHelper.php
@@ -7,6 +7,22 @@ use ZnCore\Base\Enums\EnvEnum;
 class EnvHelper
 {
 
+    public static function setErrorVisibleFromEnv(): void
+    {
+        $isDebug = EnvHelper::isDebug();
+        $level = $isDebug ? E_ALL : E_PARSE | E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR;
+        self::setErrorVisible($isDebug, $level);
+    }
+
+    public static function setErrorVisible(bool $isDebug, int $level): void
+    {
+        if ($isDebug) {
+            EnvHelper::showErrors($level);
+        } else {
+            EnvHelper::hideErrors($level);
+        }
+    }
+
     public static function showErrors(int $level = E_ALL): void
     {
         error_reporting($level);
