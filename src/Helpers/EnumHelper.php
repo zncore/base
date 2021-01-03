ac68f52f49eed0a5644cfd9da1bf57cebaae817b
Vitaliy <theyamshikov@yandex.ru>
2021-01-03 11:13:31 +0600

    upd

--- src/Helpers/EnumHelper.php
+++ src/Helpers/EnumHelper.php
@@ -2,7 +2,9 @@
 
 namespace ZnCore\Base\Helpers;
 
+use ZnCore\Base\Exceptions\NotInstanceOfException;
 use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Base\Legacy\Yii\Helpers\Inflector;
 
 class EnumHelper
 {
@@ -63,5 +65,38 @@ class EnumHelper
         ClassHelper::isInstanceOf($className, GetLabelsInterface::class, true);
         $labels = $className::getLabels();
         return $labels;
+    }
+
+    public static function getLabelsForce(string $className, array $all = null): array
+    {
+        try {
+            $labels = EnumHelper::getLabels($className);
+        } catch (NotInstanceOfException $e) {
+            $labels = null;
+        }
+        if(empty($labels)) {
+            $all = $all ?: EnumHelper::all($className);
+            $labels = array_flip($all);
+            $labels = array_map(function ($value) {
+                $value = strtolower($value);
+                return Inflector::titleize($value);
+            }, $labels);
+        }
+        return $labels;
+    }
+
+    public static function getItems(string $className): array
+    {
+        $all = EnumHelper::all($className);
+        $labels = self::getLabelsForce($className, $all);
+        $items = [];
+        foreach ($all as $name => $id) {
+            $items[] = [
+                'id' => $id,
+                'name' => mb_strtolower($name),
+                'title' => $labels[$id],
+            ];
+        }
+        return $items;
     }
 }
