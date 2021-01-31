62bc022f3684d277413c3856b16fab6774100dae
Vitaliy <theyamshikov@yandex.ru>
2021-01-31 19:24:56 +0600

    upd

---
+++ src/Helpers/DeprecateHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ZnCore\Base\Helpers;
+
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use ZnCore\Base\Exceptions\DeprecatedException;
+use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
+
+class DeprecateHelper
+{
+
+    private static $isStrictMode = false;
+    
+    public static function softThrow(string $message = '')
+    {
+        if(self::getStrictMode()) {
+            throw new DeprecatedException($message);
+        } else {
+            self::log($message);
+        }
+    }
+
+    public static function hardThrow(string $message = '')
+    {
+        throw new DeprecatedException($message);
+    }
+    
+    public static function setStrictMode() {
+        self::$isStrictMode = true;
+    }
+
+    public static function getStrictMode() {
+        return $_ENV['DEPRECATE_STRICT_MODE'] ?? self::$isStrictMode;
+    }
+    
+    private static function log(string $message = '') {
+        $container = ContainerHelper::getContainer();
+        if(!$container instanceof ContainerInterface) {
+            return;
+        }
+        if(!$container->has(LoggerInterface::class)) {
+            return;
+        }
+        /** @var LoggerInterface $logger */
+        $logger = $container->get(LoggerInterface::class);
+        $noticeMessage = 'Deprecated functional';
+        if($message) {
+            $noticeMessage .= ': ' . $message;
+        }
+        $logger->notice($noticeMessage, [
+            'message' => $message,
+        ]);
+    }
+}
