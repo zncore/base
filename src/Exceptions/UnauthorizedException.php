dbaad81e24e87c51be59eda5963ff20407fcf249
Vitaliy <theyamshikov@yandex.ru>
2021-02-02 21:53:47 +0600

    upd

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -3,7 +3,15 @@
 namespace ZnCore\Base\Exceptions;
 
 use Exception;
+use ZnCore\Base\Helpers\DeprecateHelper;
 
+DeprecateHelper::getStrictMode();
+
+/**
+ * Class UnauthorizedException
+ * @package ZnCore\Base\Exceptions
+ * @deprecated
+ */
 class UnauthorizedException extends Exception
 {
 
