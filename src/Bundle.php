a6cfac4748ce869a80a702bd1bdd4e08e327f41b
Vitaliy <theyamshikov@yandex.ru>
2021-05-05 15:05:48 +0600

    upd

---
+++ src/Bundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ZnCore\Base;
+
+use ZnCore\Base\Libs\App\Base\BaseBundle;
+
+class Bundle extends BaseBundle
+{
+
+    public function i18next(): array
+    {
+        return [
+            'core' => 'vendor/zncore/base/src/i18next/__lng__/__ns__.json',
+            'symfony' => 'vendor/zncore/base/src/Libs/I18Next/SymfonyTranslation/i18next/__lng__/__ns__.json',
+        ];
+    }
+}
