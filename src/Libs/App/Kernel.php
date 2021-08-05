267c81feec0c30926aeb46dde12a8452895893f2
Vitaliy <theyamshikov@yandex.ru>
2021-08-05 22:08:36 +0600

    upd

--- src/Libs/App/Kernel.php
+++ src/Libs/App/Kernel.php
@@ -10,7 +10,9 @@ use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
 use ZnCore\Base\Libs\App\Enums\KernelEventEnum;
 use ZnCore\Base\Libs\App\Events\LoadConfigEvent;
 use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
+use ZnCore\Base\Libs\App\Interfaces\ConfigManagerInterface;
 use ZnCore\Base\Libs\App\Interfaces\LoaderInterface;
+use ZnCore\Base\Libs\App\Libs\ConfigManager;
 use ZnCore\Base\Libs\App\Subscribers\ConfigureContainerSubscriber;
 use ZnCore\Base\Libs\App\Subscribers\ConfigureEntityManagerSubscriber;
 use ZnCore\Base\Libs\Cache\CacheAwareTrait;
@@ -53,6 +55,7 @@ class Kernel implements KernelInterface
     {
         $containerConfigurator = ContainerHelper::getContainerConfiguratorByContainer($container);
         $containerConfigurator->singleton(KernelInterface::class, $this);
+        $containerConfigurator->singleton(ConfigManagerInterface::class, ConfigManager::class);
         $this->container = $container;
     }
 
