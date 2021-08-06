c71cfdc6ad49c7aba9b1bdc49f3fcae4615c3ab2
Vitaliy <theyamshikov@yandex.ru>
2021-08-06 18:01:18 +0600

    upd

---
+++ src/Libs/Store/Drivers/Xml.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ZnCore\Base\Libs\Store\Drivers;
+
+use ZnCore\Base\Encoders\XmlEncoder;
+
+class Xml extends BaseEncoderDriver implements DriverInterface
+{
+
+    public function __construct()
+    {
+        $encoder = new XmlEncoder(true, 'UTF-8', false);
+        $this->setEncoder($encoder);
+    }
+}
