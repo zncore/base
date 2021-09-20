b0107a59d33ad8fa3e19c24d683e8f90a6a5b549
Vitaliy <theyamshikov@yandex.ru>
2021-09-20 10:43:56 +0600

    upd

--- src/Exceptions/InvalidMethodParameterException.php
+++ src/Exceptions/InvalidMethodParameterException.php
@@ -6,4 +6,17 @@ use Exception;
 
 class InvalidMethodParameterException extends Exception
 {
+
+    private $parameterName;
+
+    public function getParameterName(): ?string
+    {
+        return $this->parameterName;
+    }
+
+    public function setParameterName(string $parameterName): self
+    {
+        $this->parameterName = $parameterName;
+        return $this;
+    }
 }
