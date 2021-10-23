8786522f576c29e39cdf525ed26257d4419ef31d
Vitaliy <theyamshikov@yandex.ru>
2021-10-24 00:12:15 +0600

    upd

--- src/Helpers/EnvServerHelper.php
+++ src/Helpers/EnvServerHelper.php
@@ -5,18 +5,18 @@ namespace ZnCore\Base\Helpers;
 class EnvServerHelper
 {
 
-    public static function isEqual(string $name)
+    public static function isEqualUri(string $name)
     {
         return trim($_SERVER['REQUEST_URI'], '/') == trim($name, '/');
     }
     
-    public static function find(string $name)
+    public static function isContainsSegmentUri(string $name)
     {
         $isMatch = preg_match('/(\/' . $name . ')($|\/|\?)/', $_SERVER['REQUEST_URI'], $matches);
         return $isMatch ? $matches[1] : null;
     }
 
-    public static function fix(string $name)
+    public static function fixUri(string $name)
     {
         $_SERVER['SCRIPT_NAME'] = "/$name/index.php";
         $_SERVER['PHP_SELF'] = "/$name/index.php";
