815d1e62f079520ad65c70475ed73beb494c3047
ZnDeveloper <zndev@ya.ru>
2022-05-04 22:57:51 +0600

    upd

--- tests/Unit/FileHelperTest.php
+++ tests/Unit/FileHelperTest.php
@@ -2,23 +2,22 @@
 
 namespace ZnCore\Base\Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
 use ZnCore\Base\Helpers\FileSizeHelper;
 use ZnCore\Base\Helpers\FindFileHelper;
 use ZnCore\Base\Helpers\MimeTypeHelper;
 use ZnCore\Domain\Helpers\EntityHelper;
+use ZnTool\Test\Asserts\DataAssert;
+use ZnTool\Test\Asserts\DataTestCase;
 
-final class FileHelperTest extends TestCase
+final class FileHelperTest extends DataTestCase
 {
 
     public function testScanDirTree()
     {
         $tree = FindFileHelper::scanDirTree(__DIR__ . '/../../src/i18next');
-
         $array = EntityHelper::collectionToArray($tree);
-        $json = json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/../data/FileHelper/testScanDirTree.json'), $json);
+        $expected = $this->loadFromJsonFile(__DIR__ . '/../data/FileHelper/testScanDirTree.json');
+        $this->assertArraySubset($expected, $array);
     }
 
     public function testGetMimeTypes()
@@ -57,8 +56,19 @@ final class FileHelperTest extends TestCase
 
     public function testSize()
     {
-        $size = FileSizeHelper::sizeFormat(1022475);
+        $size = FileSizeHelper::sizeFormat(123);
+        $this->assertEquals('123 B', $size);
 
+        $size = FileSizeHelper::sizeFormat(1022475);
         $this->assertEquals('998.51 KB', $size);
+
+        $size = FileSizeHelper::sizeFormat(56461789651);
+        $this->assertEquals('52.58 GB', $size);
+
+        $size = FileSizeHelper::sizeFormat(5646178965111111);
+        $this->assertEquals('5.01 PB', $size);
+
+        $size = FileSizeHelper::sizeFormat(5999999999999999999);
+        $this->assertEquals('5.2 EB', $size);
     }
 }
