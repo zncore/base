b69c11eb73662f078405b6ad8cc8f8b240e38f66
Vitaliy <theyamshikov@yandex.ru>
2022-06-07 13:26:04 +0600

    upd

--- src/Libs/Store/Helpers/StoreHelper.php
+++ src/Libs/Store/Helpers/StoreHelper.php
@@ -7,17 +7,17 @@ use ZnCore\Base\Libs\Store\StoreFile;
 class StoreHelper
 {
 
-    public static function load($fileName = null, $key = null, $default = null)
+    public static function load($fileName = null, $key = null, $default = null, string $driver = null)
     {
-        $store = new StoreFile($fileName);
+        $store = new StoreFile($fileName, $driver);
         $data = $store->load($key);
         $data = $data !== null ? $data : $default;
         return $data;
     }
 
-    public static function save($fileName = null, $data): void
+    public static function save($fileName = null, $data, string $driver = null): void
     {
-        $store = new StoreFile($fileName);
+        $store = new StoreFile($fileName, $driver);
         $store->save($data);
     }
 }
