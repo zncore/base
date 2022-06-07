220ce6bfcd73fbe8bb54fd06de1c8dd994768478
Vitaliy <theyamshikov@yandex.ru>
2022-06-07 17:57:43 +0600

    upd

--- src/Libs/FileSystem/Helpers/FileStorageHelper.php
+++ src/Libs/FileSystem/Helpers/FileStorageHelper.php
@@ -7,6 +7,29 @@ use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
 class FileStorageHelper
 {
 
+    public static function isLocked(string $filename): bool
+    {
+        if(!file_exists($filename)) {
+            return false;
+        }
+        $fp = fopen($filename, 'r+');
+        if (!flock($fp, LOCK_EX|LOCK_NB, $wouldblock)) {
+            if ($wouldblock) {
+                // другой процесс удерживает блокировку
+                return true;
+            } else {
+                // не удалось заблокировать по другой причине, например. нет такого файла
+                throw new \Exception('не удалось заблокировать по другой причине, например. нет такого файла');
+//                return true;
+            }
+        } else {
+            // блокировка получена
+            flock($fp, LOCK_UN); // снятие блокировки
+            fclose($fp);
+            return false;
+        }
+    }
+
     public static function remove(string $path)
     {
         $path = FilePathHelper::pathToAbsolute($path);
